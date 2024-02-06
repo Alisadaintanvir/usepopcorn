efb938fe79c5bc6a4fd9120cde48de8ee6003cd2
alisadaintanvir <alisadaintanvir@gmail.com>
2024-02-06 11:31:04 +0600

    add starRating feature

---
+++ src/components/StarRating.js
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import Star from "./Star";
+
+const containerStyle = {
+  display: "flex",
+  alignItems: "center",
+  gap: "16px",
+};
+
+const starContainerStyle = {
+  display: "flex",
+};
+
+export default function StarRating({
+  maxRating = 5,
+  size = 48,
+  color = "#fcc419",
+  className = "",
+  message = [],
+}) {
+  const [rating, setRating] = useState(0);
+  const [tempRating, setTempRating] = useState(0);
+
+  function handleRating(rating) {
+    setRating(rating);
+  }
+
+  const textStyle = {
+    lineHeight: "1",
+    margin: "0",
+    fontSize: `${size / 1.5}px`,
+    color,
+  };
+
+  return (
+    <div style={containerStyle} className={className}>
+      <div style={starContainerStyle}>
+        {Array.from({ length: maxRating }, (_, i) => (
+          <Star
+            key={i}
+            onClick={() => handleRating(i + 1)}
+            onHoverIn={() => setTempRating(i + 1)}
+            onHoverOut={() => setTempRating(0)}
+            full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
+            color={color}
+            size={size}
+          />
+        ))}
+      </div>
+      <p style={textStyle}>
+        {message.length === maxRating
+          ? message[tempRating ? tempRating - 1 : rating - 1]
+          : tempRating || rating || ""}
+      </p>
+    </div>
+  );
+}
